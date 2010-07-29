40208f7b1fbbf5cd6f2b99dc6e41d84f9edc881b
Nathaniel Case <Qalthos@gmail.com>
2010-07-29 14:00:45 -0400

    Comments!

--- src/pokedex.py
+++ src/pokedex.py
@@ -17,6 +17,7 @@ class Pokedex:
         for line in nat_data:
             pokarray = line.split()
             if len(pokarray) == 3:
+                # This pokemon does not have a second type.
                 pokarray.append("---")
             self.dex.append(pokemon.Pokemon(pokarray))
         nat_data.close()
