1b4b839ebbffa739609a475b5ad0f3f66e28a371
Nathaniel Case <Qalthos@gmail.com>
2017-01-14 17:55:48 -0500

    Load data from pokeAPI project

--- pydex/gui/pokebook.py
+++ pydex/gui/pokebook.py
@@ -1,15 +1,21 @@
 import wx
 
-from pydex import regional_dex
 from pydex.gui.pokepage import PokedexPage
+from pydex.utils import table_data_gen
 
 
 class PokedexNotebook(wx.Notebook):
     def __init__(self, *args, **kwargs):
         super(PokedexNotebook, self).__init__(*args, **kwargs)
-        for dex_info in regional_dex.IDS:
-            page = PokedexPage(self, dex_info)
-            self.AddPage(page, dex_info['region'])
+        regions = list(table_data_gen('region_names'))
+        pokedexen = list(table_data_gen('pokedexes'))
+        pokedex_map = list(table_data_gen('pokemon_dex_numbers'))
+        for region in regions:
+            pokedex = [dex for dex in pokedexen if dex['region_id'] ==
+                       region['region_id']][-1]
+            pokedex_list = [int(pokemon['species_id']) for pokemon in pokedex_map if pokemon['pokedex_id'] == pokedex['id']]
+            page = PokedexPage(self, {'name': region['name'], 'region': region['name'], 'pokemon': pokedex_list})
+            self.AddPage(page, region['name'])
 
     def load(self, filename=None):
         if not getattr(self, 'config', None):
