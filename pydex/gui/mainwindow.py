071336806e00c3067799b8717e2b9810f12c316b
Nathaniel Case <Qalthos@gmail.com>
2017-01-24 19:26:44 -0500

    Hook up events

--- pydex/gui/mainwindow.py
+++ pydex/gui/mainwindow.py
@@ -4,6 +4,12 @@ import wx
 from pydex.gui.pokebook import PokedexNotebook
 
 
+# Custom IDs for reference
+ID_MISS = wx.NewId()
+ID_SEEN = wx.NewId()
+ID_HAVE = wx.NewId()
+
+
 class MainWindow(wx.Frame):
     def __init__(self):
         super(MainWindow, self).__init__(None, title='pyDex.py', size=(700, 600))
@@ -16,19 +22,23 @@ class MainWindow(wx.Frame):
         self.toolbar = self.CreateToolBar(wx.TB_NOICONS)
 
         self.new = wx.Button(self.toolbar, wx.ID_NEW, 'New...')
+        self.new.Bind(wx.EVT_BUTTON, self.new_file)
         self.toolbar.AddControl(self.new)
 
         self.open = wx.Button(self.toolbar, wx.ID_OPEN, 'Open')
+        self.open.Bind(wx.EVT_BUTTON, self.open_file)
         self.toolbar.AddControl(self.open)
 
         self.save = wx.Button(self.toolbar, wx.ID_SAVE, 'Save')
+        self.save.Bind(wx.EVT_BUTTON, self.save_file)
         self.toolbar.AddControl(self.save)
 
         self.toolbar.AddStretchableSpace()
-        self.toolbar.AddCheckTool(0, 'Missing', wx.NullBitmap)
-        self.toolbar.AddCheckTool(1, 'Seen', wx.NullBitmap)
-        self.toolbar.AddCheckTool(2, 'Caught', wx.NullBitmap)
+        self.toolbar.AddCheckTool(ID_MISS, 'Missing', wx.NullBitmap)
+        self.toolbar.AddCheckTool(ID_SEEN, 'Seen', wx.NullBitmap)
+        self.toolbar.AddCheckTool(ID_HAVE, 'Caught', wx.NullBitmap)
         self.toolbar.Realize()
+        self.toolbar.Bind(wx.EVT_TOOL, self.filter_pages)
 
         # Contents
         panel = wx.Panel(self)
@@ -38,3 +48,20 @@ class MainWindow(wx.Frame):
         vbox.Add(self.notebook, wx.EXPAND | wx.ALL)
 
         panel.SetSizer(vbox)
+
+    def new_file(self, event):
+        pass
+
+    def open_file(self, event):
+        pass
+
+    def save_file(self, event):
+        pass
+
+    def filter_pages(self, event):
+        for i, id in enumerate((ID_MISS, ID_SEEN, ID_HAVE)):
+            if event.Id == id:
+                break
+
+        self.notebook.filter ^= 2**i
+        self.notebook.refresh_all()
